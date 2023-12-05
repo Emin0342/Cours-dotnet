765ccfc1966a1a3b7db4ff90a4e9938609054a44
CORUMLU Emin <emin.corumlu03@gmail.com>
2023-12-06 00:10:08 +0100

    DOCUMENTATION

---
+++ readme.md
@@ -0,0 +1,86 @@
+## API DOTNET 8 DE GESTION DE BIBLIOTHEQUE
+
+### Introducition
+
+Ce projet est une api faite avec le framwork .NET 8.
+
+Elle permet la gestion d'une bibliothèque de livre
+
+### Prérequis 
+
+- Premierement il faut clonner le depot 
+```bash
+git clone ...
+```
+
+- Ensuite il faut telecharger tous les packages nécéssaire a son bon fonctionnement :  
+
+EntityFramework 8 : 
+
+```shell
+dotnet add package Microsoft.EntityFrameworkCore --version 8.0.0
+```
+
+Sqlite3
+```shell
+dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 8.0.0
+```
+`SUR WINDOWS IL FAUT EGALEMENT TELECHARGER L'EXECUTABLE SUR LE SITE DE SQLITE, UNE FOIS INSTALLER IL FAUT DEFINIR LA VARIBLE D'ENVIRONNEMENT `
+
+` - SUR LE MENU DEMARER CHERCHER "Modifier les varibles d'environnement système" `
+
+` - CLIKER SUR "Variables d'environnement"`
+
+` - DOUBLE CLICKER SUR "Path" PRESENT DANS VARIBLES SYSTEME PUIS "Nouveau"'`
+
+` - INSERER LE CHEMIN DE L'EXECUTABLE SQLITE3 (Exemple : C:\Users\emins\Documents\sqlite3\sqlite3 )`
+
+`POUR TESTER SI SQLITE EST BIEN INSTALLER LANCER UN CMD ET UTILISER LA COMMANDE : `
+```shell
+sqlite
+```
+
+
+EntityFramework - Design
+```shell
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 8.0.0
+```
+
+### Démarrage 
+
+placer vous dans le bon repertoir et lancer la commande :
+
+```cs
+dotnet build
+```
+afin de verifier si il n'y a pas d'erreur
+
+si aucune erreur ne détecter lancer cette commande pour utiliser l'API : 
+
+```cs
+dotnet run
+```
+
+une fois cela fait il vous suffit de vous rendre a l'URL indiqué dans le cmd : 
+
+```shell
+Now listening on: http://localhost:5123
+```
+
+Pour voir l'API de manière graphique il faut ajouter `/swagger` a la fin de l'URL
+
+# Requete GET : 
+
+Afin d'éffectuer une requete GET il faut clicker sur `GET` -->  `TRY IT OUT` puis `EXECUTE` et plus bas vous devrez voir toutes les données présente dans la BDD
+
+# Requete POST : 
+
+Afin d'éffectuer une requete POST il faut clicker sur `POST` -->  `TRY IT OUT` et plus bas vous devrez voir toutes les champs avec leur valeur définit par defaut qui peuvent être modifier puis valider le tous avec `EXECUTE`
+(pour verifier que tous a bien fonctionner il suffit simplement de faire un nouvelle requete GET (ou get selon l'id pour éviter de chercher parmis plusieur ligne) afin de voir si la nouvelle ligne s'est bien inserer)
+
+
+# Requete PUT : 
+
+Afin d'éffectuer une requete PUT il faut clicker sur `PUT` -->  `TRY IT OUT`,  preciser l'id de la ligne que l'ont veut modifier et modifier les champs juste en dessous puis faire `EXECUTE`
+
+# REQUETE DELETE : 
--- webapi3/Controllers/BookController.cs
+++ webapi3/Controllers/BookController.cs
@@ -5,8 +5,8 @@ using newWebAPI.Models;
 namespace newWebAPI.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
-public class BookController : ControllerBase
+[Route("api/[controller]")] // pour trouver dans l'url on definit le chemin d'acces --> localhost/port/api/controlleur
+public class BookController : ControllerBase 
 {
     private readonly AppDbContext _context;
 
@@ -15,43 +15,47 @@ public class BookController : ControllerBase
         _context = context;
     }
 
-    [HttpGet]
-    public async Task<IEnumerable<Book>> Get()
+    // le get (le R(read) du CRUD qui sert a afficher TOUTES les donnés)
+    [HttpGet] 
+    public async Task<IEnumerable<Book>> Get() 
     {
-        return await _context.Books.ToListAsync();
+        return await _context.Books.ToListAsync(); // retourne toutes les données
     }
 
+
+    // le get (le R(read) du CRUD qui sert a afficher les données selon l'id 
+    // on definir l'id et on a toutes les donnés que de cet id la
     [HttpGet("{id}")]
 
-    public async Task<ActionResult<Book>> Get(int id)
+    public async Task<ActionResult<Book>> Get(int id) // ici on met en paramètre int id pour que l'utilisateur donne l'id pour -->
     {
-        var book = await _context.Books.FindAsync(id);
+        var book = await _context.Books.FindAsync(id); // --> avoir toutes les données de cet id la
 
-        if (book == null)
+        if (book == null) // si il n'y a aucun livre
         {
-            return NotFound();
+            return NotFound(); // on retourne une érreur
         }
 
-        return book;
+        return book; // sinnon on retourne le livre en question
     }
 
-    [HttpPost]
+    [HttpPost] // le post (le c(create) du CRUD qui sert a inserere un nouveaux champ 
 
     public async Task Post([FromBody]Book book)
     {
-        _context.Books.Add(book);
-        await _context.SaveChangesAsync();
+        _context.Books.Add(book); // ici demande a l'utilisateur de remplir les champs
+        await _context.SaveChangesAsync(); // et on insert le champs
     }
 
-    [HttpPut("{id}")]
-    public async Task<ActionResult> PutBook(int id, [FromBody] Book book)
+    [HttpPut("{id}")] // le put (le U(update) du CRUD qui sert a mettre a jour un champ deja existant
+    public async Task<ActionResult> PutBook(int id, [FromBody] Book book) // ici on a en parametre int id pour que l'utilisateur indique selon quel id il faut faire la mise a jour de champs
     {
-        if (id != book.Id)
+        if (id != book.Id) // si le champ existe pas
         {
-            return BadRequest();
+            return BadRequest(); // on renvoie une erreur
         }
-        _context.Entry(book).State = EntityState.Modified;
-        await _context.SaveChangesAsync();
+        _context.Entry(book).State = EntityState.Modified;  // ici on demande de faire la modifcation
+        await _context.SaveChangesAsync(); // et on met a jour si sa marche 
         return Ok();
     }
     
